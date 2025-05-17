1cbfcf31dc459397f14171a74f852cabad69f50c
Truong Nguyen <truong.prototype@gmail.com>
2025-05-17 13:00:35 +0700

    Update footer and mobile style

--- wp-content/themes/doitunique/footer.php
+++ wp-content/themes/doitunique/footer.php
@@ -1,41 +1,65 @@
 <footer>
     <div class="container">
-        <div class="footer-logo">
-            <img src="<?php echo site_url(); ?>/wp-content/uploads/logo.png" alt="Logo">
+        <div class="footer-content">
+            <div class="footer-logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="DoitUnique">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+            </div>
+            <div class="footer-cols">
+                <div class="footer-col">
+                    <h4>What We Do</h4>
+                    <ul>
+                        <li><a href="#">Candle Gift</a></li>
+                        <li><a href="#">Wool Tufting</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Company</h4>
+                    <ul>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Career</a></li>
+                        <li><a href="#">Business</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Support</h4>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Policy</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Contact</h4>
+                    <ul>
+                        <li><a href="#">WhatsApp</a></li>
+                        <li><a href="#">Support 24</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="footer-cols">
-            <div>
-                <h4>What We Do</h4>
-                <ul>
-                    <li>Candle Gift</li>
-                    <li>Wool Tufting</li>
-                </ul>
-            </div>
-            <div>
-                <h4>Company</h4>
-                <ul>
-                    <li>About Us</li>
-                    <li>Career</li>
-                </ul>
-            </div>
-            <div>
-                <h4>Support</h4>
-                <ul>
-                    <li>FAQ</li>
-                    <li>Policy</li>
-                    <li>Business</li>
-                </ul>
-            </div>
-            <div>
-                <h4>Contact</h4>
-                <ul>
-                    <li>WhatsApp</li>
-                    <li>Support 24</li>
-                </ul>
-            </div>
+        
+        <div class="footer-social">
+            <a href="#" class="social-icon facebook">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+                </svg>
+            </a>
+            <a href="#" class="social-icon twitter">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
+                </svg>
+            </a>
+            <a href="#" class="social-icon linkedin">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+                    <rect x="2" y="9" width="4" height="12"></rect>
+                    <circle cx="4" cy="4" r="2"></circle>
+                </svg>
+            </a>
         </div>
+        
         <div class="footer-bottom">
-            <p>Copyright © 2024 Doitunique. All rights reserved.</p>
+            <p>Copyright © <?php echo date('Y'); ?> Doitunique. All rights reserved.</p>
         </div>
     </div>
     <?php wp_footer(); ?>
--- wp-content/themes/doitunique/header.php
+++ wp-content/themes/doitunique/header.php
@@ -15,7 +15,14 @@
         <a href="<?php echo home_url(); ?>" class="logo-link">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="DoitUnique" class="logo">
         </a>
-        <nav>
+        
+        <button class="mobile-menu-toggle">
+            <span class="toggle-bar"></span>
+            <span class="toggle-bar"></span>
+            <span class="toggle-bar"></span>
+        </button>
+        
+        <nav class="main-nav">
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">What We Serve</a></li>
@@ -24,13 +31,12 @@
                 <li><a href="#">Contact Us</a></li>
             </ul>
         </nav>
+        
         <div class="header-icons">
             <a href="#" class="instagram-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
-                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-                    <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-                </svg>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M2.5 4.99935L5 1.66602H15L17.5 4.99935M2.5 4.99935V16.666C2.5 17.108 2.67559 17.532 2.98816 17.8445C3.30072 18.1571 3.72464 18.3327 4.16667 18.3327H15.8333C16.2754 18.3327 16.6993 18.1571 17.0118 17.8445C17.3244 17.532 17.5 17.108 17.5 16.666V4.99935M2.5 4.99935H17.5M13.3333 8.33268C13.3333 9.21674 12.9821 10.0646 12.357 10.6897C11.7319 11.3148 10.8841 11.666 10 11.666C9.11594 11.666 8.2681 11.3148 7.64298 10.6897C7.01786 10.0646 6.66667 9.21674 6.66667 8.33268" stroke="#151515" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
             </a>
             <a href="#" class="language-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -42,6 +48,18 @@
         </div>
     </div>
 </header>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+    const mainNav = document.querySelector('.main-nav');
+    
+    mobileMenuToggle.addEventListener('click', function() {
+        this.classList.toggle('active');
+        mainNav.classList.toggle('show');
+    });
+});
+</script>
 
 </body>
 </html> 
